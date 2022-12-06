643cbe9c8616dacc4d69376cc9572ecf29d91655
Temur Mannonov <temur.mannonov@zazmic.com>
2022-12-06 10:57:06 +0500

    fixed client

--- client/main.go
+++ client/main.go
@@ -7,10 +7,11 @@ import (
 	arifmeticService "github.com/TemurMannonov/grpc_start/genproto/arifmetic_service"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
